486e1903def12e77f3dc65031388a17fa8cbea8a
Austin Rhoads <austinrrhoads@ymail.com>
2020-01-26 23:30:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   x = num.to_i
-  dev3 = x % 3
-  dev5 = x % 5
-  if dev3 == 0
+  div3 = x % 3
+  div5 = x % 5
+  if div3 == 0
     puts "fizz"
-  elsif dev5 == 0
+  elsif div5 == 0
     puts "buzz"
-
+  elsif div
 end
 end
 
