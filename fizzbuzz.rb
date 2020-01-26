468cc9ba32e91adbf76d1de99f22f1e04c184a08
Austin Rhoads <austinrrhoads@ymail.com>
2020-01-26 23:24:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  puts "fizz"
+  x = num.to_i
+  puts x
 end
 
 fizzbuzz(15)
+
